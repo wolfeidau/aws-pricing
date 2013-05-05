6d9a7197ab6ae23ade7e40abfc067b946bedeefd
Mark Wolfe <mark@wolfe.id.au>
2013-05-05 13:56:13 +1000

    Removed a bunch of stuff which is not needed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,27 +9,6 @@ module.exports = function(grunt) {
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // left concat here to remind people to use it at some stage for their app assets
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    // left uglify here to remind people to use it at some stage for their app assets
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
     cafemocha: {
       src: 'test/**/*.js',
       options: {
@@ -70,8 +49,6 @@ module.exports = function(grunt) {
   })
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat')
-  grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-jshint')
   grunt.loadNpmTasks('grunt-contrib-watch')
 
